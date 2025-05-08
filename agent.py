0f6b31569b51f827a3b88d057b0f82f78c146f27
oussama bouroukba <oussama.bouroukba@gmail.com>
2025-05-08 23:20:04 +0100

    add ipenAI key

--- agent.py
+++ agent.py
@@ -6,6 +6,7 @@ from apart_scrapper import get_avito_flats
 from dotenv import load_dotenv
 import re
 import os
+import streamlit as st
 
 # Load environment variables from .env file
 
@@ -32,7 +33,7 @@ tools = [
 llm = ChatOpenAI(
     temperature=0,
     model_name="gpt-3.5-turbo",
-    openai_api_key=os.getenv("OPENAI_API_KEY")
+    openai_api_key=st.secrets["OPENAI_API_KEY"]
 )
 
 # Initialize agent with ReAct style
